4a71d0dc4ddab8ab445121546225271865e321e6
Luis Urzúa <lurzua@softup.cl>
2025-12-31 18:28:43 -0300

    NUXt config actualizado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-12-05',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css:[
-    '~/assets/css/main.css'
-  ],
 
-  app:{
-    head:{
-      title: 'Mi tienda',
-      meta:[
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      title: 'Mi tienda de servicios',
+      meta: [
         {
           name: 'description',
-          content:'Bienvenido a mi tienda de regalos'
-        }
-      ]
-    }
+          content: 'Bienvenido a mi tienda de servicios generales.',
+        },
+      ],
+    },
   },
-
-
 
   modules: [
     '@nuxt/eslint',
@@ -27,16 +24,34 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/ui',
     '@prisma/nuxt',
-    'nuxt-auth-utils'
+    'nuxt-auth-utils',
   ],
 
-  
-  //Pre render de todo el sitio
-  nitro:{
-   prerender:{
-      routes:['/', '/about','/contact','/pricing','/products'],
-      ignore:['/dashboard', '/dashboard/**'],
-      crawlLinks: true
-   }
-  }
-})
+  // additional config
+  vite: {
+    resolve: {
+      alias: {
+        '.prisma/client/index-browser':
+          './node_modules/.prisma/client/index-browser.js',
+      },
+    },
+  },
+
+  // SPA - Single Page Application
+  // ssr: false,
+  // nitro: {
+  //   preset: 'static',
+  //   static: true,
+  // },
+
+  // Prender - Todo el sitio
+  nitro: {
+    prerender: {
+      routes: ['/', '/about', '/contact', '/pricing', '/products'],
+      ignore: ['/dashboard', '/dashboard/**'],
+      // Habilitar el crawling para descubrir enlaces automáticamente
+      crawlLinks: true,
+    },
+  },
+});
+
